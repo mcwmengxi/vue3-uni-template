d9ea42cd14bda31a4fb3c559aa9f814814d820df
mochunwang <mochunwang@qinsilk.com>
2023-08-18 18:21:15 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src')
+      }
+    ]
+  },
+  css: {
+    preprocessorOptions: {
+      // 因为uni.scss可以全局使用，这里根据自己的需求调整
+      scss: {
+        additionalData: '@import "./src/assets/styles/global.scss";'
+      }
+    }
+  },
 });
